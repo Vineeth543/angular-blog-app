2e6d97d8278e752246dd8ffeb108434d468d126e
Vineeth Serigar <vineeth.serigar@netcracker.com>
2023-05-02 11:55:16 +0530

    bootstrap and routing setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,9 +13,10 @@ import { ContactUsComponent } from './pages/contact-us/contact-us.component';
 import { SubscriptionFormComponent } from './subscription-form/subscription-form.component';
 import { CommentFormComponent } from './comments/comment-form/comment-form.component';
 import { CommentListComponent } from './comments/comment-list/comment-list.component';
+import { AboutUsComponent } from './pages/about-us/about-us.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, CategoryNavbarComponent, HomeComponent, SingleCategoryComponent, SinglePostComponent, TermsAndConditionComponent, ContactUsComponent, SubscriptionFormComponent, CommentFormComponent, CommentListComponent],
+  declarations: [AppComponent, HeaderComponent, CategoryNavbarComponent, HomeComponent, SingleCategoryComponent, SinglePostComponent, TermsAndConditionComponent, ContactUsComponent, SubscriptionFormComponent, CommentFormComponent, CommentListComponent, AboutUsComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
