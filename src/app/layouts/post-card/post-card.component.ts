2cdecf49510d5711a648d1c8c3ebc925543ff0b4
Vineeth Serigar <vineeth.serigar@netcracker.com>
2023-05-03 12:39:47 +0530

    design featured post

--- src/app/layouts/post-card/post-card.component.ts
+++ src/app/layouts/post-card/post-card.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-post-card',
   templateUrl: './post-card.component.html',
-  styleUrls: ['./post-card.component.less']
+  styleUrls: ['./post-card.component.less'],
 })
 export class PostCardComponent {
+  images = [
+    'https://images.freeimages.com/images/large-previews/ac9/railway-hdr-1361893.jpg',
+    'https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d',
+    'https://images.freeimages.com/images/large-previews/e5c/mill-house-1637156.jpg',
+    'https://images.freeimages.com/images/large-previews/810/highlander-1325292.jpg',
+  ];
 
+  randomImage() {
+    return this.images[Math.floor(Math.random() * this.images.length)];
+  }
 }
