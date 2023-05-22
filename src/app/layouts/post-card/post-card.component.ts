bd455a03c9ce5a45cd151dbafb92e55ae5d36a91
Vineeth Serigar <vineeth.serigar@netcracker.com>
2023-05-22 11:07:33 +0530

    bug fix

--- src/app/layouts/post-card/post-card.component.ts
+++ src/app/layouts/post-card/post-card.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-post-card',
   templateUrl: './post-card.component.html',
   styleUrls: ['./post-card.component.less'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PostCardComponent {
   images = [
