84dcc777aed09257dafc2ce6c90f9cebbff357ea
Vineeth Serigar <vineeth.serigar@netcracker.com>
2023-06-07 17:56:59 +0530

    Connected featured post to backend

--- src/app/layouts/post-card/post-card.component.ts
+++ src/app/layouts/post-card/post-card.component.ts
@@ -1,4 +1,5 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Post } from 'src/app/models/post';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-post-card',
@@ -7,14 +8,5 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PostCardComponent {
-  images = [
-    'https://images.freeimages.com/images/large-previews/ac9/railway-hdr-1361893.jpg',
-    'https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d',
-    'https://images.freeimages.com/images/large-previews/e5c/mill-house-1637156.jpg',
-    'https://images.freeimages.com/images/large-previews/810/highlander-1325292.jpg',
-  ];
-
-  randomImage() {
-    return this.images[Math.floor(Math.random() * this.images.length)];
-  }
+  @Input() postData!: { id: string; data: Post };
 }
---
+++ src/app/services/post.service.ts
@@ -0,0 +1,28 @@
+import { map } from 'rxjs/operators';
+import { Post } from '../models/post';
+import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PostService {
+  constructor(private afs: AngularFirestore) {}
+
+  loadFeaturedPost() {
+    return this.afs
+      .collection('posts', (ref) =>
+        ref.where('isFeatured', '==', true).limit(4)
+      )
+      .snapshotChanges()
+      .pipe(
+        map((actions) =>
+          actions.map((a) => {
+            const id = a.payload.doc.id;
+            const data = a.payload.doc.data() as Post;
+            return { id, data };
+          })
+        )
+      );
+  }
+}
